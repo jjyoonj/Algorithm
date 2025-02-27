22031db0b94cb4959fe9a96689e9db06d7eb5888
jjyoonj <68732657+jjyoonj@users.noreply.github.com>
2025-02-27 21:21:12 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+basket = [i for i in range(1,n+1)]
+temp=0
+for _ in range(m):
+    i, j = map(int, input().split())
+    temp = basket[i-1]
+    basket[i-1] = basket[j-1]
+    basket[j-1] = temp
+for i in range(n):
+    print(basket[i], end=' ')
